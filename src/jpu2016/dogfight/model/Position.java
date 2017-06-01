876299df0993bcf9762ec9dfb354cce6eabd2e1e
[JudicaelGuillaume] <[judicael.guillaume@viacesi.fr]>
2017-06-01 17:02:49 +0200

    Création de la classe Position

---
+++ src/jpu2016/dogfight/model/Position.java
@@ -0,0 +1,26 @@
+package jpu2016.dogfight.model;
+
+public class Position {
+	private double x;
+	private double y;
+	private double maxX;
+	private double maxY;
+	
+	public double getX() 
+	{
+		return x;
+	}
+	public void setX(double x) 
+	{
+		this.x = x;
+	}
+	public double getY() {
+		return y;
+	}
+	public void setY(double y) 
+	{
+		this.y = y;
+	}
+	
+	
+}
