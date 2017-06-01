9195d22b529ff44d6514379cfdd539d29d31b560
[JudicaelGuillaume] <[judicael.guillaume@viacesi.fr]>
2017-06-01 16:39:22 +0200

    Creation de la classe Plane et de l'interface IMobile

---
+++ src/jpu2016/dogfight/model/IMobile.java
@@ -0,0 +1,31 @@
+package jpu2016.dogfight.model;
+
+public interface IMobile {
+	
+	
+	public void move()
+	{
+		
+	}
+	
+	public void placeInArea(IArea area)
+	{
+		
+	}
+	
+	public isPlayer(int player)
+	{
+		
+	}
+	
+	public hit()
+	{
+		
+	}
+	
+	public isWeapon()
+	{
+		
+	}
+	
+}
---
+++ src/jpu2016/dogfight/model/Plane.java
@@ -0,0 +1,23 @@
+package jpu2016.dogfight.model;
+
+public class Plane {
+	int SPEED = 2;
+	int WIDTH = 100;
+	int HEIGHT = 30;
+	int player;
+	
+	public Plane (int player, Direction direction, Position position, String image)
+	{
+		
+	}
+	
+	public isPlayer(int player)
+	{
+		
+	}
+	
+	public hit()
+	{
+		
+	}
+}
